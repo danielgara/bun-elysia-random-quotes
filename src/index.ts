00f889354444d912ff3039d147d37604fb7dec27
Daniel Correa <yo@danielgara.com>
2024-04-22 13:36:06 -0500

    imp: random quotes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const phrases = [
+  "Get ready to be inspired…", 
+  "See rejection as redirection.",
+  "There is beauty in simplicity.",
+  "You can’t be late until you show up.",
+  "Maybe life is testing you. Don’t give up.",
+  "Impossible is just an opinion.",
+  "Alone or not you gonna walk forward.",
+]
+
+const app = new Elysia().get("/", () => {
+  return phrases[Math.floor(Math.random() * phrases.length)];
+}).listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
